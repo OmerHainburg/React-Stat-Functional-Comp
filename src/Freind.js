6fb9f87b1b57a1a72d2f7594d1504fe0c99f5f02
Your Name <you@example.com>
2022-01-31 12:50:03 +0200

    Freind

---
+++ src/Freind.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Friend = () => {
+  return <img
+             src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg"
+             alt=""
+                 style={
+                     {width:"300px",
+                     height:"250px"}
+                 }
+             />;
+};
+
+ export default Friend;
